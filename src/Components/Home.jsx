37a970f17cd1800f6ac6759a321ee1c358d50039
Sai Aryan Goswami <saiaryan.goswami1784@gmail.com>
2023-11-03 14:07:38 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,9 +8,9 @@ const Home = () => {
     };
 
     return (
-        <div className="bg-cover bg-bottom h-screen z-0 opacity-[1]" style={style}>
+        <div className="bg-cover bg-bottom h-screen z-0 opacity-[1]  bg-fixed bg-no-repeat shadow-lg" style={style}>
             {/* <div className="absolute inset-0" style={{ background: 'rgba(0, 0, 0, 0.5)' }}></div> Dark overlay */}
-            <nav className='flex items-center justify-between p-3 z-10 backdrop-blur-[0.6px] rounded-xl'>
+            <nav className='flex items-center justify-between p-3 z-10 backdrop-blur-[0.6px]'>
                 <div>
                     <img src={logo} className='h-[140px] w-[140px]' alt="Logo" />
                 </div>
@@ -32,10 +32,10 @@ const Home = () => {
                 </div>
             </nav>
 
-            <div className=' w-screen max-w-screen-lg text-center font-Orbitron text-5xl font-semibold text-white mx-auto mt-[12%] z-10'>
+            <div className=' w-screen max-w-screen-lg text-center font-Orbitron text-5xl font-semibold text-slate-400 mx-auto mt-[12%] z-10'>
                 <h1>WELCOME TO</h1>
             </div>
-            <div className='w-screen max-w-screen-lg text-center font-Orbitron text-8xl font-semibold text-white mx-auto mt-[2%] z-10'>
+            <div className='w-screen max-w-screen-lg text-center font-Orbitron text-8xl font-semibold backdrop-blur-[0.7px] text-white mx-auto mt-[2%] z-10'>
                 <h1>DATAVERSE</h1>
             </div>
         </div>
